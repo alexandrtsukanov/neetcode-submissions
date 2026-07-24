7b830c3127f783761ef84b34cc135eb3379ccc13
Александр Цуканов <72148625+alexandrtsukanov@users.noreply.github.com>
2026-07-24 18:01:52 +0300

    Add: remove-all-adjacent-duplicates-in-string-ii - submission-1

---
+++ Data Structures & Algorithms/remove-all-adjacent-duplicates-in-string-ii/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {string}
+     */
+    removeDuplicates(s, k) {
+        const stack = [];
+        
+        for (let i = 0; i < s.length; i += 1) {
+            if (!stack.length || stack[stack.length - 1][0] !== s[i]) {
+                stack.push([s[i], 1]);
+            } else {
+                stack[stack.length - 1][1] += 1;
+            }
+
+            if (stack[stack.length - 1][1] === k) {
+                stack.pop();
+            }
+        }
+
+        return stack.map(el => {
+            const count = el[1];
+            let str = [];
+            for (let i = 1; i <= count; i += 1) {
+                str.push(el[0]);
+            }
+            return str.join('');
+        }).join('');
+    }
+}
